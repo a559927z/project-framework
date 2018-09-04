0ee566e09abcb79babc90d109706c841ff1b9a31
jxzhang <a559927z@163.com>
2018-09-04 18:54:29 +0800

    [*]upload 解析excel

--- src/main/java/net/chinahrd/core/module/ModuleRegister.java
+++ src/main/java/net/chinahrd/core/module/ModuleRegister.java
@@ -1,6 +1,6 @@
 /**
-*net.chinahrd.core.cache
-*/
+ * net.chinahrd.core.cache
+ */
 package net.chinahrd.core.module;
 
 
@@ -22,130 +22,137 @@ import org.springframework.beans.factory.InitializingBean;
 
 /**
  * 模块注册中心
- *  子类使用
- *  @Lazy(false)
-	@Component
-	两个注解
- * 建议实现类名为 `模块名_Moudule` 或者@Component("模块名_Moudule")
+ * 子类使用
+ *
  * @author htpeng
- *2016年10月8日下午1:42:50
+ * 2016年10月8日下午1:42:50
+ * @Lazy(false)
+ * @Component 两个注解
+ * 建议实现类名为 `模块名_Moudule` 或者@Component("模块名_Moudule")
  */
+public abstract class ModuleRegister extends RegisterAbstract implements InitializingBean {
+    private static Logger log = Logger.getLogger(ModuleRegister.class);
 
-public abstract class ModuleRegister extends RegisterAbstract implements InitializingBean  {
-	private static Logger log=Logger.getLogger(ModuleRegister.class);
-	/**
-	 * 默认提供获取方法
-	 * @return 文件在包项目中的路径    建议不以  "/" 绝对路径开头
-	 */
-	protected abstract String getFilePath();
-	protected  String getXmlPath(){
-		return getFilePath();
-	}
+    /**
+     * 默认提供获取方法
+     *
+     * @return 文件在包项目中的路径    建议不以  "/" 绝对路径开头
+     */
+    protected abstract String getFilePath();
 
-	/**
-	 * 注册缓存
-	 * @return
-	 */
-	protected abstract CacheRegister registerCache();
-	
-	
-	
 
-	/**
-	 * 注册菜单
-	 * @return
-	 */
-	protected abstract MenuRegister registerMenu();
-	
-	
-	/**
-	 * 注册对外API
-	 * @return
-	 */
-	protected abstract ApiRegister registerApi();
-	
-	/**
-	 * 注册定时任务
-	 * @return
-	 */
-	protected abstract JobRegister registerJob();
-	
-	
-	
-	/**
-	 * 模块安装
-	 * @return
-	 */
-	 protected abstract ModuleService install();
-	
-	/**
-	 * 模块卸载
-	 * @return
-	 */
-	 protected abstract ModuleService uninstall();
-	
-//	
+    /**
+     * 注册缓存
+     *
+     * @return
+     */
+    protected abstract CacheRegister registerCache();
+
+
+    /**
+     * 注册菜单
+     *
+     * @return
+     */
+    protected abstract MenuRegister registerMenu();
+
+
+    /**
+     * 注册对外API
+     *
+     * @return
+     */
+    protected abstract ApiRegister registerApi();
+
+    /**
+     * 注册定时任务
+     *
+     * @return
+     */
+    protected abstract JobRegister registerJob();
+
+
+    /**
+     * 模块安装
+     *
+     * @return
+     */
+    protected abstract ModuleService install();
+
+    /**
+     * 模块卸载
+     *
+     * @return
+     */
+    protected abstract ModuleService uninstall();
+
+    @Override
+    protected String getXmlPath() {
+        return getFilePath();
+    }
+
+    //
 //	/**
 //	 * 注册Menu
 //	 * @return
 //	 */
 //	protected abstract CacheRegisterInterface registerCache();
-	/* (non-Javadoc)
-	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+    /* (non-Javadoc)
+     * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
 	 */
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		register();
-	}
-	
-	private void register() {
-		Read read=getRead(FileType.getFileType(getFilePath()));
-		InputStream ins=getInputStream(getFilePath());
-		if(null==ins){
-			return;
-		}
-		read.load(ins);
-		ModuleModel moduleModel=new ModuleModel(read.getModel());
-		ModuleManagerCenter.setMenuModle(moduleModel);
-		log.info("Loadind module:"+moduleModel);
-		
-		ApiRegister apiRegister=registerApi();
-		if(null!=apiRegister){
-			apiRegister.setModuleModel(moduleModel);
-			apiRegister.register();
-		}
-		
-		CacheRegister cacheRegisterInterface=registerCache();
-		if(null!=cacheRegisterInterface){
-			cacheRegisterInterface.setModuleModel(moduleModel);
-			cacheRegisterInterface.register();
-		}
-		
-		
-		MenuRegister menuRegisterInterface=registerMenu();
-		if(null!=menuRegisterInterface){
-			menuRegisterInterface.setModuleModel(moduleModel);
-			menuRegisterInterface.register();
-		}
-		
-		JobRegister jobRegister=registerJob();
-		if(null!=jobRegister){
-			jobRegister.setModuleModel(moduleModel);
-			jobRegister.register();
-		}
-		
-		
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        register();
+    }
+
+    private void register() {
+        Read read = getRead(FileType.getFileType(getFilePath()));
+        InputStream ins = getInputStream(getFilePath());
+        if (null == ins) {
+            return;
+        }
+        read.load(ins);
+        ModuleModel moduleModel = new ModuleModel(read.getModel());
+        ModuleManagerCenter.setMenuModle(moduleModel);
+        log.info("Loadind module:" + moduleModel);
+
+        ApiRegister apiRegister = registerApi();
+        if (null != apiRegister) {
+            apiRegister.setModuleModel(moduleModel);
+            apiRegister.register();
+        }
+
+        CacheRegister cacheRegisterInterface = registerCache();
+        if (null != cacheRegisterInterface) {
+            cacheRegisterInterface.setModuleModel(moduleModel);
+            cacheRegisterInterface.register();
+        }
+
+
+        MenuRegister menuRegisterInterface = registerMenu();
+        if (null != menuRegisterInterface) {
+            menuRegisterInterface.setModuleModel(moduleModel);
+            menuRegisterInterface.register();
+        }
+
+        JobRegister jobRegister = registerJob();
+        if (null != jobRegister) {
+            jobRegister.setModuleModel(moduleModel);
+            jobRegister.register();
+        }
+
+
 //		log.debug("Load complete module:"+moduleModel);
-		
-	}
-	
-	private Read getRead(FileType fileType){
-		switch (fileType) {
-		case XML:
-			return new MenuXmlRead();
-		case PROPS:
-			return new PropsRead();
-		}
-		return null;
-	}
+
+    }
+
+    private Read getRead(FileType fileType) {
+        switch (fileType) {
+            case XML:
+                return new MenuXmlRead();
+            case PROPS:
+                return new PropsRead();
+        }
+        return null;
+    }
 }
--- src/main/java/net/chinahrd/core/web/InitDataServlet.java
+++ src/main/java/net/chinahrd/core/web/InitDataServlet.java
@@ -13,46 +13,49 @@ import net.chinahrd.core.web.init.DataInitializationConfig;
 
 /**
  * 服务器启动后 自动加载 初始化数据的serlvet
- * 
+ *
  * @author htpeng 2016年2月2日下午2:36:13
  */
 public class InitDataServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public InitDataServlet() {
-		super();
-	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InitDataServlet() {
+        super();
+    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
 
-	public void init(ServletConfig config) throws ServletException {
-		for (Class<? extends DataInitialization> c : DataInitializationConfig.getList()) {
-			try {
-				DataInitialization d = (DataInitialization) c.newInstance();
-				d.init(config);
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        for (Class<? extends DataInitialization> c : DataInitializationConfig.getList()) {
+            try {
+                DataInitialization d = (DataInitialization) c.newInstance();
+                d.init(config);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
